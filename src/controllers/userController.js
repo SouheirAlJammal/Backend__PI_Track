407ae1f4f39ae386dce989a06ef053ef56f4dbcd
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2024-03-09 18:54:32 +0200

    changing security

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -86,7 +86,7 @@ const login = async (req, res) => {
     res.cookie("accessToken", jwtToken, {
       httpOnly: true,
       sameSite: "None",
-      secure: true, // Set to true in production (requires HTTPS)
+      secure: false, // Set to true in production (requires HTTPS)
     });
 
     return res.status(200).json({
